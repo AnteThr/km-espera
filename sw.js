f8ba71782780c0238fbf593d794ceceb90c9bc9e
Ante Trogrlić <ante@espera.hr>
2026-03-25 01:57:37 +0100

    Add files via upload
    PWA wrapper v6.1

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = 'km-espera-v6.1';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  var url = new URL(e.request.url);
+  // Only cache our own wrapper assets, never the GAS iframe content
+  if (url.origin === self.location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(function(r) {
+        return r || fetch(e.request);
+      })
+    );
+  }
+});
